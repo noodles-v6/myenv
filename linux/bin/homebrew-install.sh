297b6d8a29c0f743fc6c493190419b0a83a6750b
noodles-v6 <rungumpth000@gmail.com>
2013-07-17 13:10:24 +0800

    简化我的生活，强制一些娱乐应用的运行时间，目前监控QQ。

--- linux/bin/homebrew-install.sh
+++ linux/bin/homebrew-install.sh
@@ -1,1 +1,10 @@
+sudo chown $(whoami) /usr/local/bin
+sudo chown $(whoami) /usr/local/lib
+
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+brew install -g dos2unix
+brew link dos2unix
+
+brew install -g mysql
+brew link mysql
